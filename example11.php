c9af5786a218c3aaf124fad34a2cc6b19f5c94f4
feihu1996x <gsx0312@qq.com>
2018-08-04 14:41:52 +0800

    类型约束

---
+++ example11.php
@@ -0,0 +1,21 @@
+<?php
+/*
+* @file: example11.php
+* @brief: PHP面向对象——类型约束
+* @author: feihu1996.cn
+* @date: 上午14:35 18-8-4
+* @version: 1.0
+*/
+
+class A {
+    public function go(){
+        echo "go in A";
+        echo "\r\n";
+    }
+}
+
+function test(A $a) {  // 将参数$a限定为类A的实例
+    $a->go();
+}
+
+test(new A());
